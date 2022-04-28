fed3d932cae3307ac55ee34bcc81ca3fd344bfc1
shijuhong <shijuhong@hikvision.com.cn>
2022-04-28 16:03:41 +0800

    ParallelUglifyPlugin 多进程压缩 JS

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -8,6 +8,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const { IgnorePlugin } = require("webpack");
+const ParallelUglifyPlugin = require("webpack-parallel-uglify-plugin");
 
 module.exports = merge(webpackCommon, {
   mode: "production",
@@ -73,6 +74,26 @@ module.exports = merge(webpackCommon, {
     new IgnorePlugin({
       resourceRegExp: /^\.\/locale$/,
       contextRegExp: /moment$/,
+    }),
+    // 使用 ParallelUglifyPlugin 并行压缩输出的 JS 代码
+    new ParallelUglifyPlugin({
+      // 传递给 UglifyJS 的参数
+      uglifyJS: {
+        output: {
+          // 最紧凑的输出
+          beautify: false,
+          // 删除所有的注释
+          comments: false,
+        },
+        compress: {
+          // 删除所有的 `console` 语句，可以兼容ie浏览器
+          drop_console: true,
+          // 内嵌定义了但是只用到一次的变量
+          collapse_vars: true,
+          // 提取出出现多次但是没有定义成变量去引用的静态值
+          reduce_vars: true,
+        },
+      },
     }),
   ],
   optimization: {
